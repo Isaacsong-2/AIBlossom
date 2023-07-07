c23e431bfeec423067109a7818e5a72eb4bb55dd
SongIsak <thddltkr10@naver.com>
2023-07-07 10:56:48 +0900

    수정사항적용

--- src/main/java/com/aiblossom/entity/User.java
+++ src/main/java/com/aiblossom/entity/User.java
@@ -22,7 +22,7 @@ public class User {
     @Column(unique = true, nullable = false)
     private String username;
 
-    @Column(nullable = false)
+    @Column
     private String password;
 
     @Column
